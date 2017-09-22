5feef5bdc95d50ce02540ae26d04f122fb2220c9
Pranav Dakshinamurthy <pranav.dakshina@gmail.com>
2017-09-21 20:40:25 -0500

    add src and public

---
+++ public/js/signin.js
@@ -0,0 +1,9 @@
+var app = angular.module('myApp', []);
+
+app.controller('signInController', function($scope) {
+  // $scope.user = 'John Doe';
+  // $scope.pass = 'john.doe@gmail.com';
+  console.log($scope.user);
+  console.log($scope.pass);
+
+});
